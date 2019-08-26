21705960f01c130dd097891a71d581583859339c
Martín (Netux) Rodríguez <send.email+git@netux.site>
2019-08-26 13:51:38 -0300

    Echo last commit hash in update_static.sh

--- update_static.sh
+++ update_static.sh
@@ -3,6 +3,8 @@ rm -fr ./*
 git clone --no-checkout https://github.com/pxlsspace/Pxls temp
 cd temp
 git checkout master -- resources/public
+echo ""
+echo "Last Pxls commit hash: $(git rev-parse HEAD)"
 cd ..
 mv -uf temp/resources/public/* .
 rm -fr temp
